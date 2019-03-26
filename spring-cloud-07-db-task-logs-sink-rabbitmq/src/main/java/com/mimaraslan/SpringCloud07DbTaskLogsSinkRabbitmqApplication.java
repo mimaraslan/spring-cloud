86fc7f8cc6c5ee47299b50acf0aaad7f4b7722c7
mimaraslan <mimaraslan@gmail.com>
2019-03-26 23:29:15 +0300

    Spring Cloud
    Spring Cloud

---
+++ spring-cloud-07-db-task-logs-sink-rabbitmq/src/main/java/com/mimaraslan/SpringCloud07DbTaskLogsSinkRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.mimaraslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloud07DbTaskLogsSinkRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloud07DbTaskLogsSinkRabbitmqApplication.class, args);
+	}
+
+}
