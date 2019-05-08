54a559266c6180576f4547ebc9dc7fc4b137a16e
mimaraslan <mimaraslan@gmail.com>
2019-05-08 23:34:03 +0300

    Spring Cloud
    Spring Cloud

--- spring-cloud-03-remote-config-github/src/main/java/com/mimaraslan/MainApplication.java
+++ spring-cloud-03-remote-config-github/src/main/java/com/mimaraslan/MainApplication.java
@@ -3,13 +3,22 @@ package com.mimaraslan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+@EnableGlobalMethodSecurity(prePostEnabled=true,securedEnabled=true,jsr250Enabled=true)
 @EnableConfigServer
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/decrypt/**").authenticated().and().csrf().disable();
+         http.authorizeRequests().antMatchers("/encrypt/**").authenticated().and().csrf().disable();
+     }
 }
