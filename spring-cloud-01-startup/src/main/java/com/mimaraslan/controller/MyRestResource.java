90f888a2e40ce3bb2043554be9e994705fa73a45
mimaraslan <mimaraslan@gmail.com>
2021-09-29 23:51:04 +0300

    Spring Cloud
    Spring Cloud

--- spring-cloud-01-startup/src/main/java/com/mimaraslan/controller/MyRestResource.java
+++ spring-cloud-01-startup/src/main/java/com/mimaraslan/controller/MyRestResource.java
@@ -1,12 +1,14 @@
 package com.mimaraslan.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class MyRestResource {
 	
-	@RequestMapping(value="/message")
+	@GetMapping
 	public String SayHello(){
 		return "Hi, from Spring Cloud!";
 	}
